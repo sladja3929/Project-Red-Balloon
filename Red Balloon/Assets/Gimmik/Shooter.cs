38b4bff434a9fc68c7aae4aef78a2aeba5994fa7
WonJun Na <43125863+sladja3929@users.noreply.github.com>
2022-11-10 23:09:58 +0900

    Shooter Raycast

--- Red Balloon/Assets/Gimmik/Shooter.cs
+++ Red Balloon/Assets/Gimmik/Shooter.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
-{
+{   
     public GameObject bullet;
+    public float rayDistance = 15f;
+    public float attackDelay;
+
+    RaycastHit hit;
+    private float _time = 0f;
 
     public void Shoot()
     {
         Instantiate(bullet, transform.position, transform.rotation);
     }
 
-    private float _time = 0f;
-    public float attackDelay;
-
     void Update()
     {
+        Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.red);
         _time += Time.deltaTime;
+
         
-        if (_time > attackDelay)
+
+        if(Physics.Raycast (transform.position, transform.forward, out hit, rayDistance))
         {
-            _time = 0;
-            Shoot();
+            if (_time > attackDelay)
+            {
+                _time = 0;
+                Shoot();
+            }
         }
+        
     }
+
 }
