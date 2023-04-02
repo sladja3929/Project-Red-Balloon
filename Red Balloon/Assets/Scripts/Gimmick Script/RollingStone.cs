098b10544a20e831f0b8cdda446f3912294759de
OruteZ <creeperss85@gmail.com>
2023-04-02 21:47:41 +0900

    Rolling Stone Gimmick Script 추가, Rolling Stone Killer 태그 추가

---
+++ Red Balloon/Assets/Scripts/Gimmick Script/RollingStone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RollingStone : Gimmick
+{
+    private Rigidbody _rigidbody;
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.isKinematic = true;
+    }
+    
+    public override void Execute()
+    {
+        _rigidbody.isKinematic = false;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Player")) GameManager.Instance.KillBalloon();
+        if (other.collider.CompareTag($"Rolling Stone Killer")) Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) GameManager.Instance.KillBalloon();
+        if (other.CompareTag($"Rolling Stone Killer")) Destroy(gameObject);
+    }
+}
