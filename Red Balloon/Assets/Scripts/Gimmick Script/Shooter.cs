e36a884e565fa141dd1412fa2bf4bf2d99d3143f
OruteZ <creeperss85@gmail.com>
2023-04-10 22:50:42 +0900

    롱스톤 + 슈터 수정

--- Red Balloon/Assets/Scripts/Gimmick Script/Shooter.cs
+++ Red Balloon/Assets/Scripts/Gimmick Script/Shooter.cs
@@ -22,12 +22,14 @@ public class Shooter : Gimmick
     private void Shoot()
     {
         var transform1 = transform;
+        /*
         Debug.DrawRay(transform1.position, transform1.forward * rayDistance, Color.red);
 
 
         if (!Physics.Raycast(transform.position, transform.forward, out var hit, rayDistance)) return;
         Debug.Log(hit.collider.gameObject);
         if (!hit.collider.CompareTag("Player")) return;
+        */
         
 
         Instantiate(bullet, transform1.position, transform1.rotation);
