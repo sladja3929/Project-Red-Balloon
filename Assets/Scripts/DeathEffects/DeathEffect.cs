d917861a39ab70b5b93d28cd104fdf01ab2c0254
OZ <dwd0102@sju.ac.kr>
2023-11-10 17:09:12 +0900

    EffectManager + Shark Death Effect

---
+++ Assets/Scripts/DeathEffects/DeathEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DeathEffect : MonoBehaviour
+{
+    public abstract void Show(Vector3 position);
+    public abstract void Hide();
+
+    public abstract EffectType GetEffectType();
+}
+
+public enum EffectType
+{
+    Shark,
+    Explosion,
+    Blood
+}
