2fab37db1001c3bddbad8ff75ee8423f17c327ad
WonJun Na <43125863+sladja3929@users.noreply.github.com>
2024-08-31 14:09:37 +0900

    스테이지3 섬 기믹, 구름 마테리얼

--- Assets/Scripts/Gimmick Script/RotateIsland.cs
+++ Assets/Scripts/Gimmick Script/RotateIsland.cs
@@ -6,25 +6,29 @@ using UnityEngine;
 public class RotateIsland : MonoBehaviour
 {
     [SerializeField] private bool isLeft = false;
+    [SerializeField] private float rotateSpeed = 1;
+    [SerializeField] private float acceleration = 0.1f;
 
+    private short dir = -1;
+    private Vector3 angle;
+    
     private GameObject parent;
     // Start is called before the first frame update
     void Start()
     {
         parent = transform.parent.gameObject;
+        if (isLeft) dir = 1;
+        angle = parent.transform.eulerAngles;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (isLeft) parent.transform.eulerAngles = new Vector3(90, 0, 0);
+            //acceleration += acceleration;
+            angle.z = angle.z + dir * (rotateSpeed + acceleration);
+            parent.transform.eulerAngles = angle;
         }
     }
 }
