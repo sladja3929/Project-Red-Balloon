e8b6d114e939c7d7ecdf2529724fbe0876e6d445
WonJun Na <43125863+sladja3929@users.noreply.github.com>
2025-02-04 23:23:44 +0900

    컴파일 에러 수정

--- Assets/Scripts/UI script/PauseMenu.cs
+++ Assets/Scripts/UI script/PauseMenu.cs
@@ -94,8 +94,8 @@ public class PauseMenu : MonoBehaviour
         
         camSensitivitySlider.onValueChanged.AddListener(delegate { SetControllerSensitivity(); });
         mouseSensitivitySlider.onValueChanged.AddListener(delegate { SetMouseSensitivity(); });
-        sfxVolumeSlider.onValueChanged.AddListener(delegate {SetSfxVolume()});
-        musicVolumeSlider.onValueChanged.AddListener(delegate {SetBackgroundVolume()});
+        sfxVolumeSlider.onValueChanged.AddListener(delegate { SetSfxVolume(); });
+        musicVolumeSlider.onValueChanged.AddListener(delegate { SetBackgroundVolume(); });
     }
     
     public void SetMouseSensitivity()
