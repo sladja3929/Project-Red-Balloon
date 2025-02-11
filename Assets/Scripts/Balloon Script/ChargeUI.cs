acf060d7853b9fe4c9609352b5effa89463646e4
WonJun Na <43125863+sladja3929@users.noreply.github.com>
2025-02-12 04:30:13 +0900

    셰이더 오클루전 수정
    베타테스트 v11

--- Assets/Scripts/Balloon Script/ChargeUI.cs
+++ Assets/Scripts/Balloon Script/ChargeUI.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class ChargeUI : MonoBehaviour
 {
-    private Material _mat;
+    private Material[] _mats;
     
     private void Awake()
     {
-        _mat = transform.GetComponent<Renderer>().material;
+        List<Material> materialList = new List<Material>();
 
+        // 자신과 자식 오브젝트의 모든 Renderer 컴포넌트를 가져옴
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer rend in renderers)
+        {
+            materialList.AddRange(rend.materials);
+        }
+        
+        _mats = materialList.ToArray();
     }
     
     public void SetChargeUI(float chargeRate)
     {
-        _mat.SetFloat("_ChargeRate", chargeRate);
+        foreach (var _mat in _mats)
+        {
+            _mat.SetFloat("_ChargeRate", chargeRate);
+        }
     }
 }
